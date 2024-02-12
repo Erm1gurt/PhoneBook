db858d13122e67eb159c6b2b43c2172602330991
Erm1gurt <danil1http@yandex.ru>
2024-02-13 02:20:33 +0300

    Доработана функция move_menu(), доработана функция add_contact(), настроена функция views_contact(), созданы функции view_table() и jump_page()

--- main.py
+++ main.py
@@ -11,9 +11,54 @@ def views_contact():
     with open('database.csv', 'r', encoding='utf-8') as file:
         headers = file.readline().strip().split(';')
         file_reader = csv.reader(file, delimiter=';')
-        table = PrettyTable(headers)
-        table.add_rows(sorted(file_reader))
+        contacts = sorted(file_reader)
+    print(len(contacts))
+    table = PrettyTable(headers)
+    all_pages = len(contacts) // 10 + bool(len(contacts) % 10)
+
+    def view_table(page_number: int = 1) -> None:
+        start = (page_number != 1) * (page_number - 1) * 10
+        end = 10 * page_number
+        table.clear_rows()
+        print(start, end)
+        table.add_rows(contacts[start:end])
+        print(f'Страница {page_number} из {all_pages}')
         print(table)
+        page_menu_dict = {'-1': ['Предыдущая страница', jump_page],
+                          '0': ['Первая страница', jump_page],
+                          '1': ['Следующая страница', jump_page],
+                          '2': ['Перейти на указанную страницу', jump_page],
+                          '3': ['Последняя страница', jump_page],
+                          '4': ['Главное меню', main_menu]}
+        page_menu = [f'{key}. {value[0]}' for key, value in page_menu_dict.items()]
+        if page_number == 1:
+            page_menu_list = ' | '.join(list(page_menu[2:]))
+            page_num_list = tuple(page_menu_dict)[2:]
+        else:
+            page_menu_list = ' | '.join(list(page_menu))
+            page_num_list = tuple(page_menu_dict)
+        print(page_menu_list)
+        move = move_menu(page_num_list)
+        if move != '4':
+            page_menu_dict[move][1](move, page_number)
+        else:
+            main_menu()
+
+    def jump_page(move, previous_page):
+        if move == '-1':
+            view_table(previous_page - 1)
+        if move == '0':
+            view_table(1)
+        elif move == '1':
+            view_table(previous_page + 1)
+        elif move == '2':
+            page_list = tuple(map(str, range(1, all_pages + 1)))
+            page_number = int(move_menu(page_list, menu=False))
+            view_table(page_number)
+        elif move == '3':
+            view_table(all_pages)
+
+    view_table()
 
 
 def add_contact():
@@ -26,7 +71,7 @@ def add_contact():
     name = input('Укажите имя: ')
     patronymic = input('Укажите отчество: ')
     organisation = input('Укажите ораганизацию: ')
-    work_number = input('Укажите рабочий номер телефона')
+    work_number = input('Укажите рабочий номер телефона: ')
     phone_number = input('Укажите личный номер телефона: ')
     with open('database.csv', 'a', encoding='utf-8') as file:
         file_writer = csv.writer(file, delimiter=';', lineterminator='\r')
@@ -36,7 +81,8 @@ def add_contact():
     move = move_menu(['1', '2'])
     if move == '1':
         main_menu()
-    add_contact()
+    else:
+        add_contact()
 
 
 def edit_contact():
@@ -55,15 +101,18 @@ def search_contact():
     print('\nНайти контакт')
 
 
-def move_menu(buttons: tuple):
+def move_menu(buttons: tuple, menu=True):
     '''
     Функция для получения и проверки вводных данных от пользователя
     :return: str
     '''
-    move = input('\nУкажите номер раздела в который хотите перейти: ')
+    enter_massage = f'\nУкажите номер раздела в который хотите перейти: ' if menu else f'Укажите номер страницы: '
+    error_massage = f'Указанный раздел отсутствует, попробуйте еще раз' if menu \
+        else f'Такой страницы не существует, попробуйте еще раз'
+    move = input(enter_massage)
     while move not in buttons:
-        print('Указанный раздел отсутствует, попробуйте еще раз')
-        move = input('\nУкажите номер раздела в который хотите перейти: ')
+        print(error_massage)
+        move = input(enter_massage)
     return move
 
 
