ed28556fd48182f0bdc558076ed4413fbec8aaf6
Erm1gurt <danil1http@yandex.ru>
2024-02-12 21:55:10 +0300

    Добавлена функция move_menu()

--- main.py
+++ main.py
@@ -30,6 +30,18 @@ def search_contact():
     print('Найти контакт')
 
 
+def move_menu(buttons: tuple):
+    '''
+    Функция для получения и проверки вводных данных от пользователя
+    :return: str
+    '''
+    move = input('\nУкажите номер раздела в который хотите перейти: ')
+    while move not in buttons:
+        print('Указанный раздел отсутствует, попробуйте еще раз')
+        move = input('\nУкажите номер раздела в который хотите перейти: ')
+    return move
+
+
 def main_menu():
     '''
     Функция реализующая главное меню
@@ -38,10 +50,9 @@ def main_menu():
     func = {'1': views_contact, '2': add_contact, '3': edit_contact, '4': search_contact}
     print('Электронный справочник\n\tГлавное меню')
     print('1. Список контактов\n2. Добавить контакт\n3. Изменить контакт\n4. Найти контакт')
-    move_menu = input('\nУкажите номер раздела в который хотите перейти: ')
-    if move_menu not in list(func):
-        raise ValueError('Выбранный пункт меню отсутствует')
-    func[move_menu]()
+    move = move_menu(tuple(func))
+    func[move]()
+
 
 if __name__ == '__main__':
     main_menu()
