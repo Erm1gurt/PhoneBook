1528c0dca58ad97f9a780624a66f5fae4b925110
Erm1gurt <danil1http@yandex.ru>
2024-02-14 08:34:02 +0300

    Пересмотрено взаимодействие с БД, доработаны описания функций

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import csv
+import math
+
 from prettytable import PrettyTable
 
 
@@ -12,7 +14,9 @@ class PhoneBook:
                              '4': 'Главное меню'}
         self.headers = []
         self.contacts = []
+        self.search_contacts = []
         self.all_pages = 0
+        self.search_all_pages = 0
         self.page_menu = [f'{key}. {value}' for key, value in self.page_actions.items()]
 
     def reader(self) -> None:
@@ -25,7 +29,7 @@ class PhoneBook:
 
     def writer(self, info: list | tuple = None, flag: bool = False) -> None:
         """
-        Функция записывает информацию внесенную пользовател в БД
+        Функция записывает информацию внесенную пользователем в БД
         :param info: Cписок данных для записи
         :param flag: Флаг, указывающий, что будут внесены изменения в БД
         """
@@ -42,11 +46,11 @@ class PhoneBook:
     @staticmethod
     def move_menu(buttons: tuple | list, flag: str = 'menu') -> str:
         """
-        Функция реализующая получение информации от пользователя, о перемещении по меню, страницам справочника
+        Функция, реализующая получение информации от пользователя о перемещении по меню, страницам справочника
         или о выборе контакта для изменения
-        :param buttons: кортеж или список состоящий из "кнопок" меню или номеров страниц справочника
-        :param flag: флаг, определяющий с чем работает функция, с "кнопками" меню, номерами контактов или
-        страниц справочника
+        :param buttons: Кортеж или список состоящий из "кнопок" меню, номеров страниц справочника или контакта
+        :param flag: Флаг, определяющий с чем работает функция, с "кнопками" меню, номерами контактов или
+        страницами справочника
         :return: Строку, с желаемым действием пользователя
         """
         if flag == 'menu':
@@ -81,24 +85,24 @@ class PhoneBook:
 
     def views_contact(self) -> None:
         """
-        Функция реализующая меню постраничного вывода записей из справочника на экран
+        Функция, реализующая меню постраничного вывода записей из справочника на экран
         """
-        PhoneBook.reader(self)
-        self.all_pages = len(self.contacts) // 10 + bool(len(self.contacts) % 10)
+        self.all_pages = math.ceil(len(self.contacts) / 10)
         print('\nСписок контактов')
         PhoneBook.view_table(self, page_number=1)
 
     def add_contact(self) -> None:
         """
-        Функция реализующая добавление контактов в справочник пользователем
+        Функция, реализующая добавление контактов в справочник пользователем
         """
         print('\nДобавить контакт')
         info = PhoneBook.filling_information()
-        PhoneBook.reader(self)
         if PhoneBook.contact_match(self, info):
             print('\nТакой контакт уже существует\n')
         else:
             PhoneBook.writer(self, info)
+            self.contacts.append(info)
+            self.contacts.sort()
             print('\nКонтакт успешно добавлен')
         print('1. Вернуться в главное меню\n2. Добавить контакт')
         move = PhoneBook.move_menu(['1', '2'])
@@ -114,14 +118,13 @@ class PhoneBook:
         """
         print('\nИзменить контакт')
         print('Для изменения, найдите необходимый контакт')
-        PhoneBook.reader(self)
         data = PhoneBook.search_engine(self)
         if data:
             print('\nУкажите номер контакта, который нужно изменить: ')
             num_list = []
-            for n, elem in enumerate(data, 1):
+            for n, contact in enumerate(data, 1):
                 num_list.append(str(n))
-                print(f'{n}: {" | ".join(elem)}')
+                print(f'{n}: {" | ".join(contact)}')
             move = int(PhoneBook.move_menu(num_list, flag='contact'))
             contact = data[move - 1]
             print('Укажите данные которые нужно изменить, если поле менять не нужно, оставьте его пустым:')
@@ -131,6 +134,7 @@ class PhoneBook:
             else:
                 self.contacts.remove(contact)
                 self.contacts.append(info)
+                self.contacts.sort()
                 PhoneBook.writer(self, flag=True)
                 print('Контакт успешно изменен')
             print('1. Вернуться в главное меню\n2. Изменить контакт')
@@ -145,36 +149,38 @@ class PhoneBook:
 
     def search_contact(self) -> None:
         """
-        Функция для поиска контактов. Вызывает поисковую функцию, которая осуществляет поиск и возвращает результат.
+        Функция, реализующая меню поиска контактов.
         Полученный результат выводится на экран с помощью представления таблицы
         """
-        PhoneBook.reader(self)
         print('\nНайти контакт')
-        self.contacts = PhoneBook.search_engine(self)
-        self.all_pages = len(self.contacts) // 10 + bool(len(self.contacts) % 10)
-        PhoneBook.view_table(self, page_number=1)
+        self.search_contacts = PhoneBook.search_engine(self)
+        self.search_all_pages = math.ceil(len(self.search_contacts) / 10)
+        PhoneBook.view_table(self, page_number=1, flag=False)
 
-    def view_table(self, page_number: int) -> None:
+    def view_table(self, page_number: int, flag: bool = True) -> None:
         """
-        Функция выводящая таблицы с данными
+        Функция, выводящая таблицы с данными контактов
         :param page_number: Номер страницы, которую нужно вывести на экран
+        :param flag: Флаг, указывающий на то, будет ли выводиться вся таблица или только данные из поиска
         """
         start = (page_number - 1) * 10
         end = page_number * 10
+        all_pages = self.all_pages if flag else self.search_all_pages
+        contacts = self.contacts[start:end] if flag else self.search_contacts[start:end]
         table = PrettyTable(self.headers)
         table.clear_rows()
-        table.add_rows(self.contacts[start:end])
+        table.add_rows(contacts)
 
-        print(f'Страница {min(page_number, self.all_pages)} из {self.all_pages}: ')
+        print(f'Страница {min(page_number, all_pages)} из {all_pages}: ')
         print(table)
 
-        if self.all_pages < 2:
+        if all_pages < 2:
             page_menu_list = self.page_menu[-1:]
-            page_num_list = tuple(self.page_actions)[-1]
+            page_num_list = tuple(self.page_actions)[-1:]
         elif page_number == 1:
             page_menu_list = self.page_menu[2:]
             page_num_list = tuple(self.page_actions)[2:]
-        elif page_number == self.all_pages:
+        elif page_number == all_pages:
             page_menu_list = [elem for elem in self.page_menu if
                               'Последняя страница' not in elem and 'Следующая страница' not in elem]
             page_num_list = tuple(elem for elem in self.page_actions
@@ -189,15 +195,16 @@ class PhoneBook:
         print(page_menu_list_str)
         move = PhoneBook.move_menu(page_num_list, flag='page')
         if move != '4':
-            PhoneBook.jump_page(self, move, page_number)
+            PhoneBook.jump_page(self, move, page_number, flag=flag)
         else:
             PhoneBook.main_menu(self)
 
-    def jump_page(self, move: str, previous_page: int) -> None:
+    def jump_page(self, move: str, previous_page: int, flag: bool = True) -> None:
         """
-        Функция осуществляющая переход по страницам справочника
+        Функция, осуществляющая переход по страницам справочника
         :param move: Действие, которое хочет совершить пользователь
         :param previous_page: Номер предыдущей страницы справочника
+        :param flag: Флаг, указывающий на то, полная это таблица или поисковая
         """
         if move == '-1':
             PhoneBook.view_table(self, previous_page - 1)
@@ -210,13 +217,14 @@ class PhoneBook:
             page_number = int(PhoneBook.move_menu(page_list, flag='page'))
             PhoneBook.view_table(self, page_number=page_number)
         elif move == '3':
-            PhoneBook.view_table(self, self.all_pages)
+            all_pages = self.all_pages if flag else self.search_all_pages
+            PhoneBook.view_table(self, all_pages)
 
     def contact_match(self, new_contact: list) -> bool:
         """
         Функция, проверяющая на совпадение нового контакта с уже существующими
         :param new_contact: Список с данными о новом контакте
-        :return:
+        :return: Булевое значение, указывающие на наличие или отсутствие совпадений
         """
         return new_contact in self.contacts
 
@@ -242,10 +250,11 @@ class PhoneBook:
     @staticmethod
     def setter(info: str, field: str, flag: str = 'fio') -> str:
         """
-        Функция осуществляющая утановку значения полю
+        Функция осуществляющая уcтановку значения полю
         :param info: Строка, которая будет выведена у пользователя на экран при запросе ввода
         :param field: Старая информация о поле в справочнике, передается в случае изменения информации о контакте,
-        иначе передается False
+        иначе передается None
+        :param flag: Флаг, указывающий на тип поля, с которым осуществляется взаимодействие
         :return: Возвращает строку для поля справочника
         """
         setter_move = {'fio': [' может содержать только буквы и', str.isalpha],
@@ -266,7 +275,7 @@ class PhoneBook:
     def search_engine(self) -> list[list]:
         """
         Поисковая функция, запрашивает у пользователя данные для поиска. Поиск осуществялется на основании частичных
-        совпадений, если пользователь внес несколько параметров для поиска, то оба должны частично совпадать с любым
+        совпадений, если пользователь внес несколько параметров для поиска, то все должны частично совпадать с любым
         из имеющихся параметров в таблице (аналогичное явление в поиске можно наблюдать в телефонной книге iPhone)
         :return: Возвращает список совпадений
         """
@@ -286,4 +295,5 @@ class PhoneBook:
 
 if __name__ == '__main__':
     obj = PhoneBook()
+    obj.reader()
     PhoneBook.main_menu(obj)
